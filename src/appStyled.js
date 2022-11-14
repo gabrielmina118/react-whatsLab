48d5522a777efedbb0eeb5b273bd635dd4a8b17a
Gabriel Mina da Silva <gabrielminadasilva@MacBook-Air-de-Gabriel.local>
2022-11-14 09:39:12 -0300

    estilização

--- src/appStyled.js
+++ src/appStyled.js
@@ -4,7 +4,7 @@ import styled from "styled-components"
 export const Main = styled.div`
     background-color: #F1F4F7;
     width: 600px;
-    height: 100vh;
+    min-height: 100vh;
     margin: 0 auto;
     display: flex;
     flex-direction: column;  
